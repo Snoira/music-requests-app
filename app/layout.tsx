d282d5065ae325611d757b7c3c91d18b13ee2bd8
Nora <noravelde@gmail.com>
2026-05-21 18:32:38 +0200

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Låtönskelista",
+  description: "Var med och styr musiken på Alice och Lukas bröllopsfest!",
 };
 
 const geistSans = Geist({
